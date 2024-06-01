ba60db6530d87849228cbf1f6f813366f6c9c38e
nuggetscoot <142285926+nuggetscoot@users.noreply.github.com>
2024-05-31 19:58:57 -0500

    string practice

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -81,3 +81,32 @@ Console.WriteLine(intArray);
 //dynamic array
 int[] dynamicArray = new int[] { 1, 2, 3, 4, 5 };
 Console.WriteLine(dynamicArray);
+
+
+// essentially an array of chars
+string testString = "test";
+
+// essentially equivalent to lower case string, but has more methods
+String testStringStatic = "testStatic";
+
+string message2 = null;
+
+
+// cannot initailize value type to null
+
+// Literal vs verbatim
+string filePath = "c:\\TestFile";
+string filePathVerbatim = @"c:\TestFile";
+
+
+// Strings are immutable
+// Race condition - two parts of program try to access variable at the same time
+
+string firstName = "Poppy";
+string lastName = "Berning";
+
+//String interpolation
+Console.WriteLine($"My name is {firstName} {lastName}");
+
+//Composite formatting
+Console.WriteLine("My name is {0} {1}", firstName, lastName);
